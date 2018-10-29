c48596a158c748d0e0e5a34ae965ed309000e2ea
박병훈 <idonno2000@gmail.com>
2018-10-30 01:19:11 +0900

    여러가지 테스트중 문제점 :  maple라이브러리와 stm32라이브러리 둘중 사용을 선택해야함 차이점 : 부트로더를 사용하냐? 안하냐?               기존 예제들을 사용할 수 있는가?
    결정? : maple라이브러리 기반으로 시작
    
    --> sd 테스트를 위해서 sd.h를 인크루드했는데 동작을 안함
         row 레벨단을 수정해야 할 것으로 판단됨
        삽질좀 하게 생겼음.. 시간없는데ㅠㅠ

--- STM32duino/rtos_blink/rtos_blink.ino
+++ STM32duino/rtos_blink/rtos_blink.ino
@@ -1,9 +1,22 @@
+#include <SD.h>
+
+#define SDCARD_SS_PIN PB12
+#define SDCARD_MOSI_PIN PB15
+#define SDCARD_MISO_PIN PB14
+#define SDCARD_SCK_PIN PB13
+
 //#include <wirish/wirish.h>
 //#include "libraries/FreeRTOS/MapleFreeRTOS.h"
 #include <MapleFreeRTOS900.h>
 
-static void vLEDFlashTask(void *pvParameters) {
-    for (;;) {
+
+static void vLEDFlashTask(void *pvParameters) 
+{
+  vTaskDelay(1000);
+   Serial.println("LEDFlash Task...\n");
+    pinMode(PC13, OUTPUT);
+    for (;;)
+    {
       vTaskDelay(850);      digitalWrite(PC13, LOW);
       vTaskDelay(50);        digitalWrite(PC13, HIGH);
       vTaskDelay(50);        digitalWrite(PC13, LOW);
@@ -11,12 +24,49 @@ static void vLEDFlashTask(void *pvParameters) {
     }
 }
 
-void setup() {
-    // initialize the digital pin as an output:
-    pinMode(PC13, OUTPUT);
-    xTaskCreate(vLEDFlashTask,"Task1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2,  NULL);
+Sd2Card card;
+static void vSDCardReadTask(void *pvParameters) 
+{
+  vTaskDelay(1000);
+  Serial.println("SD Read Task...\n");
+  Serial.print("\nInitializing SD card...\n");
+  
+  if (!card.init(SPI_HALF_SPEED, PB12)) 
+  {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the chipSelect pin to match your shield or module?");
+    while (1);
+  } 
+  else 
+  {
+    Serial.println("Wiring is correct and a card is present.");
+  }
 
-    
+  Serial.println();
+  Serial.print("Card type:         ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:      Serial.println("SD1");      break;
+    case SD_CARD_TYPE_SD2:      Serial.println("SD2");      break;
+    case SD_CARD_TYPE_SDHC:     Serial.println("SDHC");     break;
+    default:      Serial.println("Unknown");}
+
+  for (;;)
+  {
+  
+  }
+}
+
+void setup() 
+{
+   Serial.begin(115200);
+   Serial.println("Generic STM32F103C8 with bootloader...\r\n");
+   //delay(1000); 
+   
+   xTaskCreate(vLEDFlashTask,"Task1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2,  NULL);
+   xTaskCreate(vSDCardReadTask,"Task2", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2,  NULL);
+
     vTaskStartScheduler();
 }
 
