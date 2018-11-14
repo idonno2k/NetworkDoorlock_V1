1a97cc4a82b24519d4c8129a9b1825810158f3b3
박병훈 <idonno2000@gmail.com>
2018-11-14 22:20:02 +0900

    회로도 + 코드일부

--- bodychannel/bodychannel.ino
+++ bodychannel/bodychannel.ino
@@ -20,42 +20,25 @@ void setup()
     delay(1000);
     
     Serial.println(F("Generic STM32F103C8 with bootloader...\r\n"));
-    
-    pinMode(PC13, OUTPUT);
 
     vSDCardSpi2ReadTask_setup();    delay(1000);    
+    vSDCardSyncDateLoad();          delay(1000);
+    
     vPN532Serial3Task_setup();      delay(1000);
-   // vSDCardSyncDateLoad();          delay(1000);
-    //vEnc28j60spi1Task_setup();      delay(1000);
+    vEnc28j60spi1Task_setup();      delay(1000);
 
    //xTaskCreate(vPN532Serial3Task,"Task3", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1,  NULL);
    //xTaskCreate(vEnc28j60spi1Task,"Task4", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 3,  NULL);
    //vTaskStartScheduler();
-
+    pinMode(PC13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() 
 {
 
- // vEnc28j60spi1Task();
-  vPN532Serial3Task(); 
- // if(etherStep == SyncIdle)
-  //{
-  //  vPN532Serial3Task(); 
- // }
-  //vSDCardSpi2ReadTask();
+	vEnc28j60spi1Task();
 
-
-}
-
-
-void setEvent(uint8_t* event ,uint8_t event_code)
-{
-  *event = *event | event_code;
-}
-void clearEvent(uint8_t* event ,uint8_t event_code)
-{
-  *event = *event  & ~event_code;
+	vPN532Serial3Task(); 
 
 }
